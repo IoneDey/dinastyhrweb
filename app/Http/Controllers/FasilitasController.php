0f39ad33740c2eccbbe3805cd62182b1e12aa4a0
Dedy Setiawan <ione.dey@gmail.com>
2024-07-18 10:54:05 +0700

    makan 1

---
+++ app/Http/Controllers/FasilitasController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FasilitasController extends Controller {
+    //
+    public function makan() {
+        return view('fasilitas.makan');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/fasilitas/makan', [FasilitasController::class, 'makan']);
