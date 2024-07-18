41f35585c89c5aa05f06679c74dd15a66a5a8624
Dedy Setiawan <ione.dey@gmail.com>
2024-07-18 11:08:45 +0700

    makan 3

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/fasilitas/makan', [FasilitasController::class, 'makan']);
+Route::get('fasilitas/makan', [FasilitasController::class, 'makan']);
